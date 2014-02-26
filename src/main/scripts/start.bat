743a04257518814dc9d49fcef763cf47d606dfd6
clongo <none@none>
2014-02-26 15:14:14 +0100

    Feat. start scripts check now if java version is greater than 1.8

--- src/main/scripts/start.bat
+++ src/main/scripts/start.bat
@@ -3,7 +3,20 @@ rem @ <-- workaround for http://jira.codehaus.org/browse/MRESOURCES-104
 @cd %~dp0
 rem @ <-- workaround for http://jira.codehaus.org/browse/MRESOURCES-104
 
-set JAVA="%JAVA8_HOME%\bin\java.exe"
+set JAVA=%JAVA_HOME%
+if "%JAVA%" == "" (
+    echo "Java path is not configured."
+    echo "Please define JAVA_HOME environement variable or edit Java path into this script."
+    pause
+    exit
+)
 
-cd ./bin
-%JAVA% -jar ${project.artifactId}.jar
+for /f tokens^=2-5^ delims^=.-_^" %%j in ('"%JAVA%\bin\java.exe" -fullversion 2^>^&1') do set "JAVA_VER=%%j%%k"
+if %JAVA_VER% LSS 18 (
+    echo "This program requires Java version greater than 1.8."
+    echo "Please define JAVA_HOME environement variable or edit Java path into this script."
+    pause
+    exit
+)
+
+"%JAVA%\bin\java.exe" -jar ./bin/Wall-T.jar
--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -2,7 +2,24 @@
 
 cd $(dirname "$0")
 
-JAVA=$JAVA8_HOME/bin/java
+JAVA=$JAVA_HOME
+if [ -z "$JAVA" ]
+then
+        echo "Java path is not configured."
+        echo "Please define JAVA_HOME environement variable or edit Java path into this script."
+        exit
+fi
+
+
+JAVA_VER=$($JAVA/bin/java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+if [ "$JAVA_VER" -ge 18 ] 
+then
+        echo "Checking Java version: It's Ok." 
+else 
+        echo "This program requires Java version greater than 1.8."
+        echo "Please define JAVA_HOME environement variable or edit Java path into this script."
+        exit
+fi
 
 cd ./bin
-$JAVA -jar ${project.artifactId}.jar
+$JAVA/bin/java -jar ./bin/Wall-T.jar
