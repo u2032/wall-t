cd198f0b666cbc7bdac58c48ec0ee3154e8bc184
Cédric L. <clongo@ankama.com>
2014-02-27 08:52:54 +0100

    Misc. Quotes are not required for echo command in batch script

--- src/main/scripts/start.bat
+++ src/main/scripts/start.bat
@@ -5,16 +5,16 @@ rem @ <-- workaround for http://jira.codehaus.org/browse/MRESOURCES-104
 
 set JAVA=%JAVA_HOME%
 if "%JAVA%" == "" (
-    echo "Java path is not configured."
-    echo "Please define JAVA_HOME environement variable or edit Java path into this script."
+    echo Java path is not configured.
+    echo Please define JAVA_HOME environement variable or edit Java path into this script.
     pause
     exit
 )
 
 for /f tokens^=2-5^ delims^=.-_^" %%j in ('"%JAVA%\bin\java.exe" -fullversion 2^>^&1') do set "JAVA_VER=%%j%%k"
 if %JAVA_VER% LSS 18 (
-    echo "This program requires Java version greater than 1.8."
-    echo "Please define JAVA_HOME environement variable or edit Java path into this script."
+    echo This program requires Java version greater than 1.8.
+    echo Please define JAVA_HOME environement variable or edit Java path into this script.
     pause
     exit
 )
